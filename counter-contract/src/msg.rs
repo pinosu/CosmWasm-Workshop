29c786e5c944ef990cc480f0383dd2ed1d8098bb
Pino' Surace <pino.surace@live.it>
2024-12-02 13:20:56 +0100

    Add msgs

--- counter-contract/src/msg.rs
+++ counter-contract/src/msg.rs
@@ -1,11 +1,26 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
 
 #[cw_serde]
-pub struct InstantiateMsg {}
+pub struct InstantiateMsg {
+    Zero,
+    Set(u8),
+}
 
 #[cw_serde]
-pub enum ExecuteMsg {}
+pub enum ExecuteMsg {
+    Inc,
+    Dec,
+    Set(u8),
+}
 
 #[cw_serde]
 #[derive(QueryResponses)]
-pub enum QueryMsg {}
+pub enum QueryMsg {
+    #[returns(CounterResponse)]
+    Value,
+}
+
+#[cw_serde]
+pub struct CounterResponse {
+    pub value: u8,
+}
